c7c3ef79c0186ff9b2071530d756b69c8871c7e2
Haojie Zhu <zhuhj@umich.edu>
2025-04-08 16:33:09 -0400

    update cosim main file

---
+++ examples/main_cosim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Start Redis server
+redis-server &
+
+# Start the TeraSim server
+python examples/main.py &
+
+# Wait for the TeraSim server to start
+sleep 30
+
+# Start the Carla server
+python examples/carla/main.py --terasim_config /app/examples/simulation_Mcity_carla_config.yaml --map_name=McityMap_Main --control_cav
